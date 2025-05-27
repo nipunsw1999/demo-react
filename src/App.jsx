f424dbe6e48fb0ed70d21eb94dc10264ce8816ed
Nipun Weerasinghe <nipun@Nipuns-MacBook-Pro.local>
2025-05-27 17:55:00 +0530

    path updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/demo-react" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
